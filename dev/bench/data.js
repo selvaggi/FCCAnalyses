1ecfe9943ed82d2f7694f538bade9cbf1003efca
github-action-benchmark <github@users.noreply.github.com>
2022-08-03 20:30:26 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for c2a95c0f279a9954e2dd731313f464c7b7413c58

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1659558625077,
+  "lastUpdate": 1659558626851,
   "repoUrl": "https://github.com/selvaggi/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -45,6 +45,45 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/test"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "michele.selvaggi@uclouvain.be",
+            "name": "Michele Selvaggi",
+            "username": "selvaggi"
+          },
+          "committer": {
+            "email": "michele.selvaggi@uclouvain.be",
+            "name": "Michele Selvaggi",
+            "username": "selvaggi"
+          },
+          "distinct": true,
+          "id": "c2a95c0f279a9954e2dd731313f464c7b7413c58",
+          "message": "first commit advanced example with dummy methods",
+          "timestamp": "2022-08-03T22:19:31+02:00",
+          "tree_id": "bc017cbef6561c8ff860d037d49898ca7d4ce1f3",
+          "url": "https://github.com/selvaggi/FCCAnalyses/commit/c2a95c0f279a9954e2dd731313f464c7b7413c58"
+        },
+        "date": 1659558626060,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 7.778825986495164,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
+            "value": 6.260951205707018,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/test",
+            "value": 7.9790048295922995,
+            "unit": "Evt/s"
           }
         ]
       }
