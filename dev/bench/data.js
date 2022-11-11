d672390dcd7d8affb4770946ec1e855515042d18
github-action-benchmark <github@users.noreply.github.com>
2022-11-11 18:07:12 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 9786d17a3eded2bd8d1f5cf755ec75d0487ce58e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1668190030840,
+  "lastUpdate": 1668190032271,
   "repoUrl": "https://github.com/selvaggi/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -1636,6 +1636,50 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/test"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "michele.selvaggi@uclouvain.be",
+            "name": "Michele Selvaggi",
+            "username": "selvaggi"
+          },
+          "committer": {
+            "email": "michele.selvaggi@uclouvain.be",
+            "name": "Michele Selvaggi",
+            "username": "selvaggi"
+          },
+          "distinct": true,
+          "id": "9786d17a3eded2bd8d1f5cf755ec75d0487ce58e",
+          "message": "set reference time to MC truth hard vertex",
+          "timestamp": "2022-11-11T18:55:53+01:00",
+          "tree_id": "feb9d8b4365d2dc4e10dc5a99ec8557c78ba7aed",
+          "url": "https://github.com/selvaggi/FCCAnalyses/commit/9786d17a3eded2bd8d1f5cf755ec75d0487ce58e"
+        },
+        "date": 1668190031868,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts",
+            "value": 7.206001418486359,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 3.1046173172165976,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
+            "value": 5.8731251312483215,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test",
+            "value": 5.722826723047024,
+            "unit": "Evt/s"
           }
         ]
       }
