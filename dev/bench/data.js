3b48767612d6cd4acebbc6113331c67019ccb434
github-action-benchmark <github@users.noreply.github.com>
2022-11-28 14:01:45 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 5698c82af11e6d4ee901a03e4e0ddb0d99f2d0cd

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1669544091905,
+  "lastUpdate": 1669644105580,
   "repoUrl": "https://github.com/selvaggi/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -2928,6 +2928,58 @@ window.BENCHMARK_DATA = {
             "name": "prod | Events processed per second: examples/FCCee/test",
             "value": 8.762009993106423,
             "unit": "Evt/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "michele.selvaggi@uclouvain.be",
+            "name": "Michele Selvaggi",
+            "username": "selvaggi"
+          },
+          "committer": {
+            "email": "michele.selvaggi@uclouvain.be",
+            "name": "Michele Selvaggi",
+            "username": "selvaggi"
+          },
+          "distinct": true,
+          "id": "5698c82af11e6d4ee901a03e4e0ddb0d99f2d0cd",
+          "message": "updated to latest whizard training",
+          "timestamp": "2022-11-28T14:52:13+01:00",
+          "tree_id": "c76c19a180c3e6df0837838ca79d7669c3e1935c",
+          "url": "https://github.com/selvaggi/FCCAnalyses/commit/5698c82af11e6d4ee901a03e4e0ddb0d99f2d0cd"
+        },
+        "date": 1669644103922,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Time spent running the analysis: case-studies/analysis_example/scripts",
+            "value": 10.551615238189697,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: case-studies/analysis_example/scripts"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 11.43297028541565,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/higgs/mH-recoil/mumu"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/flavour/Bc2TauNu",
+            "value": 16.050065279006958,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/test",
+            "value": 11.91349482536316,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/test"
           }
         ]
       }
