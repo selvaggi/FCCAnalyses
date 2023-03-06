271bd848b164504665bd6dfb3f7cd068787705e4
github-action-benchmark <github@users.noreply.github.com>
2023-03-06 12:16:24 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for f38332654bcbd0889f2cac3098cec5a445ace4ec

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678104982077,
+  "lastUpdate": 1678104984066,
   "repoUrl": "https://github.com/selvaggi/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -6018,6 +6018,60 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "michele.selvaggi@uclouvain.be",
+            "name": "Michele Selvaggi",
+            "username": "selvaggi"
+          },
+          "committer": {
+            "email": "michele.selvaggi@uclouvain.be",
+            "name": "Michele Selvaggi",
+            "username": "selvaggi"
+          },
+          "distinct": true,
+          "id": "f38332654bcbd0889f2cac3098cec5a445ace4ec",
+          "message": "added smearing examples",
+          "timestamp": "2023-03-06T13:06:47+01:00",
+          "tree_id": "7aec3fd19820131603d21e111467b82e073f2e40",
+          "url": "https://github.com/selvaggi/FCCAnalyses/commit/f38332654bcbd0889f2cac3098cec5a445ace4ec"
+        },
+        "date": 1678104983331,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: examples/FCCee/weaver/analysis_inference.py",
+            "value": 2.3445119356074873,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 6.919685644671057,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 6.077700038090826,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 5.257454584161849,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 6.9313591838207165,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 3.7235196069668697,
+            "unit": "Evt/s"
           }
         ]
       }
