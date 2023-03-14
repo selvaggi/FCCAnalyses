720c25e706b8f10d556aba364bb813ec2fc44536
github-action-benchmark <github@users.noreply.github.com>
2023-03-14 16:56:54 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 9310683884bbf3fb2f90657d261b85cdb608117f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678813011268,
+  "lastUpdate": 1678813014480,
   "repoUrl": "https://github.com/selvaggi/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -8058,6 +8058,60 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "michele.selvaggi@cern.ch",
+            "name": "Michele Selvaggi",
+            "username": "selvaggi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9310683884bbf3fb2f90657d261b85cdb608117f",
+          "message": "Merge pull request #3 from selvaggi/my-fix-branch\n\nMerge pull request #248 from selvaggi/master",
+          "timestamp": "2023-03-14T17:42:42+01:00",
+          "tree_id": "0da496458534fd36b1f8c3c6ed9f37c790624414",
+          "url": "https://github.com/selvaggi/FCCAnalyses/commit/9310683884bbf3fb2f90657d261b85cdb608117f"
+        },
+        "date": 1678813013348,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: examples/FCCee/weaver/analysis_inference.py",
+            "value": 1.976937690851216,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 7.231144949328589,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 6.410506575344304,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 4.78825499922833,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 6.380451591573784,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 3.3639199553511694,
+            "unit": "Evt/s"
           }
         ]
       }
