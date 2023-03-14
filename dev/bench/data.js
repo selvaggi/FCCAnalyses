0e76403725c9d38c253e352e8b47b7eb6c6c7250
github-action-benchmark <github@users.noreply.github.com>
2023-03-14 16:47:54 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for a2a387c2e04d2b88a35c0cc261eb3c105f67fe31

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678812472757,
+  "lastUpdate": 1678812474512,
   "repoUrl": "https://github.com/selvaggi/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -7458,6 +7458,60 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a2a387c2e04d2b88a35c0cc261eb3c105f67fe31",
+          "message": "Merge pull request #249 from BNL-FCCee/Add_Jet_Fcn\n\nInvariant masses from vector of jets",
+          "timestamp": "2023-03-14T16:07:24+01:00",
+          "tree_id": "af74cf9703dcd34ffd600ac36d148e1912469272",
+          "url": "https://github.com/selvaggi/FCCAnalyses/commit/a2a387c2e04d2b88a35c0cc261eb3c105f67fe31"
+        },
+        "date": 1678812473929,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: examples/FCCee/weaver/analysis_inference.py",
+            "value": 2.8196738574298212,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 9.665491313809627,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 8.146162877965535,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 6.450142734050241,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 8.366102514946592,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 4.403114437037692,
+            "unit": "Evt/s"
           }
         ]
       }
