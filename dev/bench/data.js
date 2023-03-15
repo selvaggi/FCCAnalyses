08e659754ba0434097f6ceaa00c9c36e15a2b05f
github-action-benchmark <github@users.noreply.github.com>
2023-03-15 05:29:41 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 5f2566f31bd484c35574acfbeee672075b3c802a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678858179679,
+  "lastUpdate": 1678858181496,
   "repoUrl": "https://github.com/selvaggi/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -8298,6 +8298,60 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5f2566f31bd484c35574acfbeee672075b3c802a",
+          "message": "Merge pull request #248 from selvaggi/master\n\nSmearing object utilities",
+          "timestamp": "2023-03-14T17:15:54+01:00",
+          "tree_id": "d71f2057255e1f441ac24883a1544f3fc50b6c51",
+          "url": "https://github.com/selvaggi/FCCAnalyses/commit/5f2566f31bd484c35574acfbeee672075b3c802a"
+        },
+        "date": 1678858180891,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/weaver/analysis_inference.py",
+            "value": 2.5467004692262147,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 8.613526116663541,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 7.779092746067382,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 6.2285822615052835,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 7.874720438585256,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 4.4252402982425325,
+            "unit": "Evt/s"
           }
         ]
       }
